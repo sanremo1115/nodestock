8adf98d8452b748c690a99e50359a8aabd942454
cheikh <cheikhibra.fall3@gmail.comm>
2019-11-06 21:15:00 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const exphbs  = require('express-handlebars');
+const path = require('path');
+
+const PORT = process.env.PORT || 5000; 
+
+// set handlebars Middleware
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+
+// set handlebars route
+app.get('/', function (req, res) {
+    res.render('home',{
+        stuff:"this is stuff..."
+
+    });
+});
+
+//set static folder
+app.use(express.static(path.join(__dirname,'public')));
+
+app.listen(PORT ,()=> console.log(' Server listening on port' + PORT));
